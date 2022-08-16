187713d20c9ac191a2d499be15bbf2f87473f76c
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-16 00:32:14 -0700

    added tailwind settings

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cyan: "#2acfcf",
+        "dark-violet": "#3b3054",
+        red: "#f46262",
+        gray: "#bfbfbf",
+        "grayish-violet": "#9e9aa7",
+        "very-dark-blue": "#35323e",
+        "very-dark-violet": "#232127",
+      },
+    },
+  },
+  plugins: [],
+};
