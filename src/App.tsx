b19d8f22ea63acbcb273b5b26d354698099dde06
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-17 02:14:17 -0700

    added all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Statistics from "./components/Statistics";
 
 function App() {
   return (
-    <div className="">
+    <div className="h-screen w-screen overflow-x-hidden">
       <Navbar />
+      <Hero />
+      <Statistics />
+      <Banner />
+      <Footer />
     </div>
   );
 }
