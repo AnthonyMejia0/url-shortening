0df8d55e4457341b7362d46d412f2917b3f7e2d2
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-18 01:55:51 -0700

    added scrollbar hide

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Statistics from "./components/Statistics";
+import UrlDisplay from "./components/UrlDisplay";
 
 function App() {
   return (
-    <div className="h-screen w-screen overflow-x-hidden">
+    <div className="h-screen w-screen overflow-x-hidden scrollbar-hide">
       <Navbar />
       <Hero />
+      <UrlDisplay />
       <Statistics />
       <Banner />
       <Footer />
